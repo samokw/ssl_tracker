d6c5f2b34e8e7486e6b748b5f0ae05ceb2e07737
Chibuzor Okwusiuno <cokwusiu@uoguelph.ca>
2025-06-27 13:10:46 -0400

    added type helper functions

--- server/internal/types/types.go
+++ server/internal/types/types.go
@@ -1,5 +1,39 @@
 package types
 
+import "fmt"
+
 type UserID uint
 
 type DomainID uint
+
+// UserID helper functions
+func NewUserID(id uint) UserID {
+	return UserID(id)
+}
+
+func (u UserID) Uint() uint {
+	return uint(u)
+}
+
+func ValidateUserID(userID UserID) error {
+	if userID == 0 {
+		return fmt.Errorf("user ID cannot be zero")
+	}
+	return nil
+}
+
+// DomainID helper functions
+func NewDomainID(id uint) DomainID {
+	return DomainID(id)
+}
+
+func (d DomainID) Uint() uint {
+	return uint(d)
+}
+
+func ValidateDomainID(domainID DomainID) error {
+	if domainID == 0 {
+		return fmt.Errorf("domain ID cannot be zero")
+	}
+	return nil
+}
